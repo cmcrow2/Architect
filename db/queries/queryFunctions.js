0658fbf44b03e6c488fc34c43e149a713d0f4cde
Cameron Crow <19ccrow99@gmail.com>
2021-05-11 12:10:53 -0500

    Added dotenv

--- db/queries/queryFunctions.js
+++ db/queries/queryFunctions.js
@@ -1,8 +1,15 @@
 const { Pool } = require('pg');
-const info = require('../../config.js');
+const info = require('../../.env');
 const moment = require('moment');
+require('dotenv').config();
 
-const pool = new Pool(info);
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATA,
+  port: process.env.DB_PORT
+});
 
 // GET HELPERS
 const getReviews = (id, count, callback, sorter) => {
